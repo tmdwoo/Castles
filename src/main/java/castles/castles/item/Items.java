07eeb4a722612c77ce1f6d7dd148669e0ae34aa3
tmdwoo <hanseungwoo05@gmail.com>
2023-04-25 20:15:42 +0900

    [Feature] Can Create Castle with Core item

--- src/main/java/castles/castles/item/Items.java
+++ src/main/java/castles/castles/item/Items.java
@@ -3,6 +3,7 @@ package castles.castles.item;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -13,7 +14,7 @@ public class Items {
     public static ItemStack getItemCore() {
         ItemStack core = new ItemStack(Material.BLAZE_SPAWN_EGG, 1);
         ItemMeta meta = core.getItemMeta();
-        meta.displayName(Component.text("Build Castle"));
+        meta.displayName(Component.text("Build Castle", NamedTextColor.GOLD, TextDecoration.BOLD));
         meta.setCustomModelData(1);
         List<Component> lore = List.of(new TextComponent[]{Component.text("Right click to build a castle", NamedTextColor.GRAY)});
         meta.lore(lore);
