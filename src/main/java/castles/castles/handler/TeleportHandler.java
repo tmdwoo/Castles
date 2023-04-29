1522cff32f6bc15fbdda8b9f33ac8c749f1ea1e9
tmdwoo <hanseungwoo05@gmail.com>
2023-04-29 23:53:48 +0900

    [Etc] Changed !isSameLocation to isDifferentLocation

--- src/main/java/castles/castles/handler/TeleportHandler.java
+++ src/main/java/castles/castles/handler/TeleportHandler.java
@@ -15,14 +15,14 @@ import static castles.castles.localization.Phrase.TELEPORT_CANCELLED;
 
 public class TeleportHandler implements Listener {
 
-    private boolean isSameLocation(Location loc1, Location loc2) {
+    private boolean isDifferentLocation(Location loc1, Location loc2) {
         return loc1.getX() == loc2.getX() && loc1.getY() == loc2.getY() && loc1.getZ() == loc2.getZ();
     }
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
-        if (teleportWarmup.containsKey(player) && !isSameLocation(event.getFrom(), event.getTo())) {
+        if (teleportWarmup.containsKey(player) && isDifferentLocation(event.getFrom(), event.getTo())) {
             teleportWarmup.get(player).cancel();
             teleportWarmup.remove(player);
             player.sendMessage(Component.text(TELEPORT_CANCELLED.getPhrase(player), NamedTextColor.RED));
@@ -32,7 +32,7 @@ public class TeleportHandler implements Listener {
     @EventHandler
     public void onPlayerTeleport(PlayerTeleportEvent event) {
         Player player = event.getPlayer();
-        if (teleportWarmup.containsKey(player) && !isSameLocation(event.getFrom(), event.getTo())) {
+        if (teleportWarmup.containsKey(player) && isDifferentLocation(event.getFrom(), event.getTo())) {
             teleportWarmup.get(player).cancel();
             teleportWarmup.remove(player);
             player.sendMessage(Component.text(TELEPORT_CANCELLED.getPhrase(player), NamedTextColor.RED));
